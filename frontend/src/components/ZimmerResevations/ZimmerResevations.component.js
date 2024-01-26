36d4158a20e318638a0f9c84c0b13e21b3957a2e
Zidane Aiman <101519771+aimanzi@users.noreply.github.com>
2024-01-27 00:32:38 +0200

    core/adding key property to zimmerRezevation component

--- frontend/src/components/ZimmerResevations/ZimmerResevations.component.js
+++ frontend/src/components/ZimmerResevations/ZimmerResevations.component.js
@@ -4,22 +4,23 @@ import "./ZimmerResevations.component.css";
 import { Card } from "react-bootstrap";
 import { ArrowReturnRight } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
+import { addresevation } from "../redux/clientResevation";
 
 const ZimmerResevations = () => {
   const [resevation, setResevation] = useState([]);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch;
   useEffect(() => {
     const fetching = async () => {
       await fetch("http://localhost:5000/resevation/zimmerResevations")
         .then((Response) => Response.json())
         .then((data) => {
           setResevation(data.resevations);
-          // dispatch(addresevation(myresevation));
         });
     };
     fetching();
   }, []);
+  dispatch(addresevation(resevation));
 
   const BackToMainPage = () => {
     navigate("/mainpage");
@@ -37,20 +38,24 @@ const ZimmerResevations = () => {
       <div className="resevation-container">
         {resevation.map((item) => {
           return (
-            <div key={item._id} className="clientRes-container">
+            <div className="clientRes-container" key={item.zimmerName}>
               <Card>
                 <Card.Header>
                   <h6>{item.zimmerName}</h6>
                 </Card.Header>
-                {item.zimmerUnitResevation.map((resv) => {
+                {item.zimmerUnitResevation.map((resv, index) => {
+                  console.log(resv);
                   return (
-                    <Card.Body>
-                      <h6>Client Name: {resv.clientName}</h6>
-                      <h6>Zimmer Price: {resv.zimmerPrice}</h6>
-                      <h6>Zimmer Duration: {resv.duration}</h6>
-                      <h6>Start Day: {resv.startDate}</h6>
-                      <h6>End Day: {resv.endDate}</h6>
-                    </Card.Body>
+                    <div key={index}>
+                      <Card.Body>
+                        <h6>Client Name: {resv.clientName}</h6>
+                        <h6>Zimmer Duration: {resv.duration}</h6>
+                        <h6>Price Per Night: {resv.zimmerPrice}</h6>
+                        <h6>Zimmer Amount: {resv.amount}</h6>
+                        <h6>Start Day: {resv.startDate}</h6>
+                        <h6>End Day: {resv.endDate}</h6>
+                      </Card.Body>
+                    </div>
                   );
                 })}
               </Card>
